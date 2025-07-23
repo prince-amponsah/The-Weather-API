00a8c42eb1e3195d5dd58d6218ea91594fa82b95
Prince <princekwes@gmail.com>
2025-07-23 11:51:57 +0000

    APi Code and Ignore

---
+++ weather.py
@@ -0,0 +1,33 @@
+import requests
+class City:
+    def __init__(self, name_city, latitude, longtitude, units='metric'):
+        self.name_city = name_city
+        self.latitude = latitude
+        self.longtitude = longtitude
+        self.units = units
+        self.get_data()
+        self.give_info()
+
+
+    def get_data(self):
+        try:
+            results = requests.get(f"https://api.openweathermap.org/data/2.5/weather?units={self.units}&lat={self.latitude}&lon={self.longtitude}&appid=77faabcc09b1d617fb78f0462bb2ead8")
+
+        except:
+             print("Error Loading Data from Url")
+        response = results.json()
+        self.temp = response['main']['temp']
+        self.temp_min = response['main']['temp_min']
+        self.temp_max = response['main']['temp_max']
+
+
+    def give_info(self):
+        print(f"Current Temperature in Accra is Between: {self.temp_min}° and {self.temp_max}° degree Celcius")
+
+my_city = City("Accra","5.5593","0.1974")
+my_city.give_info()
+        
+
+
+
+
