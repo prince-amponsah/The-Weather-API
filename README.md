fe2f08ab43acbf14e60c56970330b04019564b0d
Prince <princekwes@gmail.com>
2025-07-23 13:51:24 +0000

    updated api

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-A simple Weather API
+A simple Weather API that utilises the openweatherapi.org api to provide weather update for a particular city:
+Information Needed to Function well;
+1. City name
+2. Latitude of the city
+3. Longtitude of City
+4. Api Token(Provided By openweatherapi.org after you create an account)
--- weather.py
+++ weather.py
@@ -1,17 +1,17 @@
 import requests
 class City:
-    def __init__(self, name_city, latitude, longtitude, units='metric'):
+    def __init__(self, name_city, latitude, longtitude, unit, token):
         self.name_city = name_city
         self.latitude = latitude
         self.longtitude = longtitude
-        self.units = units
+        self.units = unit
+        self.token = token
         self.get_data()
         self.give_info()
 
-
     def get_data(self):
         try:
-            results = requests.get(f"{url with api key}")
+            results = requests.get(f"https://api.openweathermap.org/data/2.5/weather?units={self.units}&lat={self.latitude}&lon={self.longtitude}&appid=77faabcc09b1d617fb78f0462bb2ead8")
 
         except:
              print("Error Loading Data from Url")
@@ -24,7 +24,13 @@ class City:
     def give_info(self):
         print(f"Current Temperature in Accra is Between: {self.temp_min}° and {self.temp_max}° degree Celcius")
 
-my_city = City("Accra","5.5593","0.1974")
+
+name_city = input('Please, Enter Your City \n')
+latitude = input('Enter Your Latitude \n')
+longtitude = input('Enter your Longtitude \n')
+unit = input('Please, Enter Your Unit \n')
+token = input('Please, Enter Your Token \n')
+my_city = City(name_city, latitude, longtitude,unit, token)
 my_city.give_info()
         
 
